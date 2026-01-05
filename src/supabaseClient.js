1f70d9833ff65b810144eae80848855fe26b82a3
Mohammad Alman Farooqui <almanfarooqui5@gmail.com>
2026-01-05 23:16:02 +0530

    logs desc

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// DEBUGGING LOGS: These will appear in your VS Code terminal and browser console
+// DEBUGGING LOGs
 console.log("Supabase URL:", supabaseUrl);
 console.log("Supabase Anon Key:", supabaseAnonKey ? "Loaded" : "NOT LOADED or EMPTY");
 
